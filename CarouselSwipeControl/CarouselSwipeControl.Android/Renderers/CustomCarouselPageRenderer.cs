4cc5b28fa07056b6410229b2d49e56006893d9ca
Furkan Bozdağ <Furkan Bozda>
2017-07-08 00:20:33 +0300

    Add project files.

---
+++ CarouselSwipeControl/CarouselSwipeControl.Android/Renderers/CustomCarouselPageRenderer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Android.Views;
+using CarouselSwipeControl;
+using CarouselSwipeControl.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomCarouselPage), typeof(CustomCarouselPageRenderer))]
+
+namespace CarouselSwipeControl.Droid.Renderers
+{
+    public class CustomCarouselPageRenderer : CarouselPageRenderer
+    {
+        private float x1 { get; set; }
+        private float x2 { get; set; }
+
+        private float y1 { get; set; }
+        private float y2 { get; set; }
+
+        private CustomCarouselPage _page = null;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CarouselPage> e)
+        {
+            _page = e.NewElement as CustomCarouselPage;
+            base.OnElementChanged(e);
+        }
+
+        public override bool DispatchTouchEvent(MotionEvent e)
+        {
+            // Left or right swipe control
+            if (!_page.IsSwipping)
+            {
+                if (e.ActionMasked == MotionEventActions.Down)
+                {
+                    x1 = e.GetX();
+                    y1 = e.GetY();
+
+                    return base.DispatchTouchEvent(e);
+                }
+
+                x2 = e.GetX();
+                y2 = e.GetY();
+
+                float xSize = Math.Abs(x1 - x2);
+                float ySize = Math.Abs(y1 - y2);
+
+                // Left or right swipe
+                if (xSize > ySize)
+                {
+                    return false;
+                }
+                // Up or down swipe
+                else
+                {
+                    return base.DispatchTouchEvent(e);
+                }
+            }
+
+            return base.DispatchTouchEvent(e);
+        }
+    }
+}
---
+++ CarouselSwipeControl/CarouselSwipeControl/CustomCarouselPage.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+
+namespace CarouselSwipeControl
+{
+    public class CustomCarouselPage : CarouselPage
+    {
+        // Constructor
+        public CustomCarouselPage()
+        {
+            Title = "Swipe Control";
+
+            Children.Add(new ContentPage() {BackgroundColor = Color.Coral});
+            Children.Add(new ContentPage() {BackgroundColor = Color.DarkSlateBlue});
+            Children.Add(new ContentPage() {BackgroundColor = Color.GreenYellow});
+
+            ToolbarItem toolbarItem = new ToolbarItem("Swipe", null, ToolbarItemClicked);
+            ToolbarItems.Add(toolbarItem);
+        }
+
+        private async void ToolbarItemClicked()
+        {
+            var answer = await DisplayActionSheet("Choose a selection", "Cancel", "",
+                "Enable",
+                "Disable");
+
+            if (answer == "Enable")
+                IsSwipping = true;
+            if (answer == "Disable")
+                IsSwipping = false;
+        }
+
+        public static readonly BindableProperty IsSwippingProperty = BindableProperty.Create(nameof(IsSwipping),
+            typeof(bool), typeof(CustomCarouselPage), default(bool));
+
+        public bool IsSwipping
+        {
+            get { return (bool) GetValue(IsSwippingProperty); }
+            set { SetValue(IsSwippingProperty, value); }
+        }
+    }
+}
